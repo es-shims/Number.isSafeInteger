dd03bb86f42fb1819418b3efce1ad01fe53dba40
Jordan Harband <ljharb@gmail.com>
2021-10-02 21:53:48 -0700

    [Deps] update `es-abstract`

--- implementation.js
+++ implementation.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var IsInteger = require('es-abstract/2020/IsInteger');
+var IsIntegralNumber = require('es-abstract/2021/IsIntegralNumber');
 var abs = Math.abs;
 var firstUnsafeInteger = 0x20000000000000; // 2 ** 53
 
 module.exports = function isSafeInteger(value) {
-	return IsInteger(value) && abs(value) < firstUnsafeInteger;
+	return IsIntegralNumber(value) && abs(value) < firstUnsafeInteger;
 };
